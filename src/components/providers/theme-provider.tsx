2a01523d59020af088d7c79557626f1781692ce7
0xmonas <saystupidshit@gmail.com>
2026-03-11 00:42:40 +0300

    theme fix

--- src/components/providers/theme-provider.tsx
+++ src/components/providers/theme-provider.tsx
@@ -16,7 +16,7 @@ const ThemeContext = createContext<{
 
 export function ThemeProvider({
   children,
-  defaultTheme = 'dark',
+  defaultTheme = 'light',
 }: ThemeProviderProps) {
   const [theme, setTheme] = useState<Theme>(defaultTheme);
 
