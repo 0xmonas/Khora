a1ae9bc160dd229072900866fa8739126b4ea143
0xmonas <saystupidshit@gmail.com>
2026-04-11 14:40:43 +0300

    Remove contracts from git tracking

--- contracts/contracts/v2/interfaces/IBOOARenderer.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
-
-interface IBOOARenderer {
-    function tokenURI(uint256 tokenId) external view returns (string memory);
-    function renderSVG(bytes memory bitmap) external pure returns (string memory);
-}
